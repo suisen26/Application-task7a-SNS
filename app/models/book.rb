6205383b31b15f2f221488432bb6020f67a53206
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-02-01 12:43:32 +0000

    [Initial] 応用課題7aのSNSを学ぼう

---
+++ app/models/book.rb
@@ -0,0 +1,29 @@
+class Book < ApplicationRecord
+
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  # favoritesモデルを介して、userモデルのデータを持ってくる
+  has_many :favorited_users, through: :favorites, source: :user
+
+  validates :title, presence:true
+  validates :body, presence:true, length: { maximum:200}
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
+  #検索方法分岐
+  def self.search_for(content, method)
+    if method == "perfect"
+      Book.where(title: content)
+    elsif method == "forward"
+      Book.where("title LIKE ?", content + "%")
+    elsif method == "backward"
+      Book.where("title LIKE ?", "%" + content)
+    else
+      Book.where("title LIKE ?", "%" + content + "%")
+    end
+  end
+
+end
